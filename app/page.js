686b3a542f08a9ef2900470fe60eb9c0b9a59b9d
agustin <agustinlaurito@hotmail.com>
2023-09-12 11:56:04 -0300

    Design

--- app/page.js
+++ app/page.js
@@ -37,65 +37,80 @@ function Page() {
 
 
 
-			<div className="w-full h-screen flex flex-col items-center bg-black">
+			<div className="w-full min-h-screen flex flex-col items-center bg-black pb-12">
 
 				<span className="text-6xl text-light mt-16 mb-8">
 					Experience
 				</span>
 
-				<div className="w-1/2 bg-gray rounded-3xl p-5 flex flex-row gap-5 mt-12">
+				<div className="w-3/4 bg-gray rounded-full py-5 pl-5 pr-16 flex flex-row gap-5 mt-12 items-center">
+
+					<div className="bg-light rounded-full p-5">
+						<PiSuitcaseSimpleThin color="#0D0D0D" size={80} />
+					</div>
+
 					<div className="bg-white rounded-full flex justify-center items-center w-1" />
 
 					<div className="flex flex-col">
-						<span className="text-4xl text-white font-mono">
+						<span className="text-2xl text-white font-mono">
 							Sweeppea
 						</span>
-						<span className="text-xl text-slate-400 font-mono">
+						<span className="text-lg text-slate-400 font-mono">
 							Nov. 2022 - Present
 						</span>
-						<span className="text-2xl text-white my-5">
+						<span className="text-xl text-white my-5">
 							Software Engineer
 						</span>
-						<span className="text-md text-white font-mono">Development of multiple applications for company management using Node.js and MongoDB.</span>
+						<span className="text-sm text-white font-mono">Development of multiple applications for company management using Node.js and MongoDB.</span>
 					</div>
 				</div>
 
-				<div className="w-1/2 bg-gray rounded-3xl p-5 flex flex-row gap-5 mt-12">
+				<div className="w-3/4 bg-gray rounded-full py-5 pl-5 pr-16 flex flex-row gap-5 mt-12 items-center">
+
+					<div className="bg-light rounded-full p-5">
+						<PiSuitcaseSimpleThin color="#0D0D0D" size={80} />
+					</div>
+
 					<div className="bg-white rounded-full flex justify-center items-center w-1" />
 
 					<div className="flex flex-col">
-						<span className="text-4xl text-white font-mono">
+						<span className="text-2xl text-white font-mono">
 							Finket
 						</span>
-						<span className="text-xl text-slate-400 font-mono">
+						<span className="text-lg text-slate-400 font-mono">
 							Jan 2022 - March 2023
 						</span>
-						<span className="text-2xl text-white my-5">
+						<span className="text-xl text-white my-5">
 							Software Engineer
 						</span>
-						<span className="text-md text-white font-mono">Development of financial solutions using ReactJS, Express and AWS as core technologies.</span>
+						<span className="text-sm text-white font-mono">Development of financial solutions using ReactJS, Express and AWS as core technologies.</span>
 					</div>
 				</div>
 
-				<div className="w-1/2 bg-gray rounded-3xl p-5 flex flex-row gap-5 mt-12">
-					<div className="bg-white rounded-full flex justify-center items-center w-4" />
+				<div className="w-3/4 bg-gray rounded-full py-5 pl-5 pr-16 flex flex-row gap-5 mt-12 items-center">
+
+					<div className="bg-light rounded-full p-5">
+						<PiSuitcaseSimpleThin color="#0D0D0D" size={80} />
+					</div>
+
+					<div className="bg-white rounded-full flex justify-center items-center w-1" />
 
 					<div className="flex flex-col">
-						<span className="text-4xl text-white font-mono">
+						<span className="text-2xl text-white font-mono">
 							Acquatron
 						</span>
-						<span className="text-xl text-slate-400 font-mono">
+						<span className="text-lg text-slate-400 font-mono">
 							Nov. 2020 - Jan 2022
 						</span>
-						<span className="text-2xl text-white my-5">
+						<span className="text-xl text-white my-5">
 							Software Developer
 						</span>
-						<span className="text-md text-white font-mono">My main task was the development of customized software solutions for the management of the company. Using C++ and MySQL I designed a comprehensive software for stock management and equipment production. This software not only eliminated the need to keep inventory manually, but also maximized efficiency in defining daily production and order fulfillment.</span>
+						<span className="text-sm text-white font-mono">My main task was the development of customized software solutions for the management of the company. Using C++ and MySQL I designed a comprehensive software for stock management and equipment production. This software not only eliminated the need to keep inventory manually, but also maximized efficiency in defining daily production and order fulfillment.</span>
 					</div>
 				</div>
 			</div>
 
-			<div className="w-full h-screen flex flex-col items-center bg-black">
+			<div className="w-full h-screen flex flex-col items-center bg-white">
 
 				<span className="text-6xl text-light mt-16 mb-8">
 					Personal Work
